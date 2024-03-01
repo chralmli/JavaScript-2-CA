4cac565ff870ea6cd51675829c246228fa535807
Christian Almli <chrisalm97@gmail.com>
2024-03-01 15:42:01 +0100

    post image and avatar handling

--- feed/js/feed.js
+++ feed/js/feed.js
@@ -8,6 +8,10 @@ const renderPosts = (posts) => {
         posts.forEach(post => {
             const avatarUrl = post.avatar && post.avatar.url ? post.avatar.url : 'https://i.ibb.co/b56xqf9/default-avatar.png';
             const avatarAlt = post.avatar && post.avatar.alt ? post.avatar.alt : 'Default avatar';
+
+            const postImageUrl = post.media && post.media.url ? post.media.url : 'https://i.ibb.co/cbMzC91/placeholder-img-ai.png';
+            const postImageAlt = post.media && post.media.alt ? post.media.alt : 'Default post image';
+            
             const postElement = document.createElement('div');
             postElement.className = 'col-md-4 mb-4';
             postElement.innerHTML = `
@@ -16,6 +20,7 @@ const renderPosts = (posts) => {
                     <div class="card-body">
                         <h5 class="card-title">${post.title}</h5>
                         <p class="card-text">${post.content}</p>
+                        <img src="${postImageUrl}" class="card-img" alt="${postImageAlt}">
                         <div class="post-meta">
                             <span class="likes"><i class="fas fa-thumbs-up"></i> ${post.likes} Likes</span>
                             <span class="comments"><i class="fas fa-comment"></i> ${post.comments} Comments</span>
